77ef046d1ced6fed8067673cf6bdaf0e2a346d14
Devollox <anisin.97@bk.ru>
2026-06-12 11:27:46 +0300

    feat: add notification banner

--- src/app/(home)/hero-section.tsx
+++ src/app/(home)/hero-section.tsx
@@ -5,12 +5,36 @@ import Stats from '@components/stats'
 import RenderBackdropAnimation from '@lib/render-backdrop'
 import styles from './hero-section.module.css'
 
+export function NotificationBanner() {
+	return (
+		<div className={styles.notification_banner}>
+			<div className={styles.notification_glow} />
+			<div className={styles.notification_dot} />
+			<div className={styles.notification_content}>
+				<span className={styles.notification_title}>
+					Total downloads exceeded <strong>400</strong>!
+				</span>
+				<span className={styles.notification_subtitle}>
+					Thank you for your support! We appreciate every one of you. Wishing
+					you an amazing experience with Void Presence! <br /> Check out our new{' '}
+					<a href='/schedule/downloads' className={styles.notification_link}>
+						downloads schedule
+					</a>
+					!
+				</span>
+			</div>
+		</div>
+	)
+}
+
 export default function HeroSection() {
 	return (
 		<section id='home-hero-section' className={styles.page_section}>
 			<RenderBackdropAnimation />
 			<div className={styles.page_section_inner}>
 				<div id='hero-top-container' className={styles.flex_container}>
+					<NotificationBanner />
+
 					<div className={styles.title_wrapper}>
 						<h1 className={styles.title}>
 							<span className={styles.title_gradient}>Void</span>
@@ -21,6 +45,7 @@ export default function HeroSection() {
 					<p className={`${styles.text_center} ${styles.title_description}`}>
 						Advanced Discord Rich Presence manager with customizable profiles.
 					</p>
+
 					<Button />
 					<Stats />
 					<div className={styles.mouse_icon}>
