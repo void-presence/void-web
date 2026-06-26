496b81b8678657583137799af7cbfdb42f71885d
Devollox <anisin.97@bk.ru>
2026-06-26 15:22:36 +0300

    chore: rename status nav link to statuses

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -37,7 +37,7 @@ export default function Navbar() {
 								Presence
 							</Link>
 							<Link className={styles.nav_configs_item} href='/statuses'>
-								Status
+								Statuses
 							</Link>
 						</div>
 					</div>
